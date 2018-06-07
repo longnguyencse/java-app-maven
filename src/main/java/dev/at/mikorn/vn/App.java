c80651f09dcabaf2049dbd94badf64bedb712075
Longbkit <longnguyenqu@gmail.com>
2018-06-07 17:07:48 +0700

    more step

--- src/main/java/dev/at/mikorn/vn/App.java
+++ src/main/java/dev/at/mikorn/vn/App.java
@@ -1,14 +1,43 @@
 package dev.at.mikorn.vn;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Author Mikorn vietnam
  * Created on 05-Jun-18.
  */
 
 public class App {
+    private static Logger logger = LogManager.getLogger(App.class);
+
 
     public static void main(String[] args) {
-        // check argument
+
+        logger.info(System.getProperty("user.dir"));
+
+        // Create and load default properties
+        Properties defaultProps = new Properties();
+        try {
+            FileInputStream in = new FileInputStream("./src/main/resources/configuration/bootstrap.conf");
+            defaultProps.load(in);
+            defaultProps.entrySet().forEach(objectObjectEntry -> {
+                logger.info(objectObjectEntry.getKey() + " : " + objectObjectEntry.getValue());
+            });
+
+            in.close();
+            // create application properties with default
+            Properties applicationPros = new Properties(defaultProps);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
+
 }
