32b71c597e5ca94cd2a9e89a615c43973659edc8
Longbkit <longnguyenqu@gmail.com>
2018-06-12 14:53:03 +0700

    config folder

--- src/main/java/dev/at/mikorn/vn/App.java
+++ src/main/java/dev/at/mikorn/vn/App.java
@@ -3,10 +3,10 @@ package dev.at.mikorn.vn;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.Scanner;
 
 /**
  * Author Mikorn vietnam
@@ -19,27 +19,45 @@ public class App {
 
     public static void main(String[] args) {
 
-        logger.info(System.getProperty("user.dir"));
+        logger.info(String.format("Currency work space: %s", System.getProperty("user.dir")));
 
+
+        App app = new App();
+//        app.getFileInDirectory(new File("D:\\learn\\data\\example"));
+        app.fetchProperties();
+
+        /**System.out.println("Press Any Key To Continue...");
+        new Scanner(System.in).nextLine();**/
+    }
+
+    private void getFileInDirectory(File filePath) {
+        if (null == filePath) {
+            return;
+        }
+        if (filePath.isFile()) {
+            // todo with file
+            logger.info("file" + filePath);
+            return;
+        }
+        for (File fileEx: filePath.listFiles()) {
+            getFileInDirectory(fileEx);
+        }
+    }
+
+    private void fetchProperties() {
         // Create and load default properties
         Properties defaultProps = new Properties();
         try {
-            FileInputStream in = new FileInputStream("./src/main/resources/conf/bootstrap.conf");
+            FileInputStream in = new FileInputStream("./conf/bootstrap.conf");
             defaultProps.load(in);
             defaultProps.entrySet().forEach(objectObjectEntry -> {
                 logger.info(objectObjectEntry.getKey() + " : " + objectObjectEntry.getValue());
             });
 
             in.close();
-            // create application properties with default
-            Properties applicationPros = new Properties(defaultProps);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println("Press Any Key To Continue...");
-        new Scanner(System.in).nextLine();
     }
-
 }
